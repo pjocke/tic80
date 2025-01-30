bc84e146408cba743a1b41672cbfa791b3ac5139
Joakim Bomelin <joakim.bomelin@king.com>
2025-01-30 15:05:33 +0100

    add Lissajous curve test

---
+++ lissajous.py
@@ -0,0 +1,55 @@
+# title:   game title
+# author:  game developers, email, etc.
+# desc:    short description
+# site:    website link
+# license: MIT License (change this to your license of choice)
+# version: 0.1
+# script:  python
+
+import math
+
+counter = 0
+
+def TIC():
+    global counter
+
+    cls(0)
+
+    # Phase-shifted Lissajous curve with evenly-spaced X mapping
+    t = (counter%240 / (240 - 1)) * (2 * math.pi)
+    x = int(120 * math.sin(t - (math.pi / 2)) + 120)
+    y = int(68 * math.sin(2 * (t - (math.pi / 2))))
+
+    circ(x, 68+y, 20, 12)
+
+    counter += 1#direction
+
+# <TILES>
+# 001:eccccccccc888888caaaaaaaca888888cacccccccacc0ccccacc0ccccacc0ccc
+# 002:ccccceee8888cceeaaaa0cee888a0ceeccca0ccc0cca0c0c0cca0c0c0cca0c0c
+# 003:eccccccccc888888caaaaaaaca888888cacccccccacccccccacc0ccccacc0ccc
+# 004:ccccceee8888cceeaaaa0cee888a0ceeccca0cccccca0c0c0cca0c0c0cca0c0c
+# 017:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
+# 018:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
+# 019:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
+# 020:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
+# </TILES>
+
+# <WAVES>
+# 000:00000000ffffffff00000000ffffffff
+# 001:0123456789abcdeffedcba9876543210
+# 002:0123456789abcdef0123456789abcdef
+# </WAVES>
+
+# <SFX>
+# 000:000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000304000000000
+# </SFX>
+
+# <TRACKS>
+# 000:100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+# </TRACKS>
+
+# <PALETTE>
+# 000:1a1c2c5d275db13e53ef7d57ffcd75a7f07038b76425717929366f3b5dc941a6f673eff7f4f4f494b0c2566c86333c57
+# </PALETTE>
+
